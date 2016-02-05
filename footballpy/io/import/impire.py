5f13bd7e152b7c843405511d6c6b8cc60429dec1
robertreingit <git@robertrein.de>
2016-02-05 11:03:49 +0100

    Restructured module

--- footballpy/io/import/impire.py
+++ footballpy/io/import/impire.py
@@ -11,8 +11,6 @@ impire_parser : Module which provides parsing function for Soccer
 @license: MIT
 @version 0.1
 """
-
-from __future__ import print_function
 import xml.sax, xml.sax.handler
 import numpy as np
 import pdb
@@ -55,6 +53,7 @@ class MatchInformationParser(xml.sax.handler.ContentHandler):
                 raise NameError("Couldn't determine role")
         elif name == 'player':
             self.inPlayer = True
+
         elif name == "player-metadata" and self.inTeam:
             pid = attrs['player-key']
             trikot = int(attrs['uniform-number'])
@@ -67,6 +66,7 @@ class MatchInformationParser(xml.sax.handler.ContentHandler):
                         "trikot": trikot,
                         "position": position
                     }
+
         elif name == 'name' and self.inPlayer:
             name = attrs['nickname']
             self.currentPlayer['name'] = name
@@ -102,6 +102,7 @@ def read_in_position_data(fname):
     Args:
     Returns:
     """
+
     # MAGIC NUMBERS
     _MISSING_ = -10000.0
     NO_PLAYER = 11
@@ -155,7 +156,6 @@ def read_in_position_data(fname):
 
     return home_team, guest_team, ball, half_time_id
 
-
 def split_positions_into_game_halves(pos,ht,ball):
     """ splits the data frames into first and second halves.
         Args:
@@ -177,7 +177,6 @@ def split_positions_into_game_halves(pos,ht,ball):
 
     return res
 
-
 def sort_position_data(pos,id=1):
     """Sorts the position data according to player and period.
 
@@ -191,7 +190,6 @@ def sort_position_data(pos,id=1):
     for pid in unique_player:
         res.append(pos[pos[:,:,id]==pid])
     return res
-
 
 def read_stadium_dimensions_from_pos(fname):
     """Gets the stadium specifications from the pos file.
@@ -208,7 +206,6 @@ def read_stadium_dimensions_from_pos(fname):
     length = float(specs_string[1])
     width = float(specs_string[2])
     return dict(length=length ,width=width)
-
 
 def combine_position_with_role(pos, team):
     """Combines the position data with the players role and pid data.
@@ -231,7 +228,6 @@ def combine_position_with_role(pos, team):
                     player[:,(0,2,3)],
                     trikot_to_role[trikot]))
     return res
-
 
 def run(data_path, fname_specs, fname_pos):
     """Driver function to run data loading.
@@ -276,10 +272,13 @@ def run(data_path, fname_specs, fname_pos):
 #######################################
 if __name__ == "__main__":
     
-    data_path = 'test/impire/'
-    fname_specs = 'vistrack-matchfacts-123456.xml'
-    fname_pos =  '123456.pos'
-   
+#    data_path = '../data/neu/impire/130649/'
+#    fname_specs = 'vistrack-matchfacts-130649.xml'
+#    fname_pos =  '130649.pos'
+    data_path = '../data/neu/impire/130330/'
+    fname_specs = 'vistrack-matchfacts-130330.xml'
+    fname_pos =  '130330.pos'
+
     print("Parsing match information")
     mip = MatchInformationParser()
     fname_match = data_path + fname_specs
@@ -293,5 +292,4 @@ if __name__ == "__main__":
     home_2s = sort_position_data(home_2)
     
     pos_data_home_1 = combine_position_with_role(home_1s,teams['home'])
-    pos_data, ball, match, teams = run(data_path, fname_specs, fname_pos)
-
+    pos_data, ball_data, match, teams = run(data_path, fname_specs, fname_pos)

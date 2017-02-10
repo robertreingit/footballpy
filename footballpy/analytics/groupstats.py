b9039f4effa0391db07dace123376db6fa81a862
robertreingit <git@robertrein.de>
2017-02-10 22:17:25 +0100

    Added function for team width and length.

--- footballpy/analytics/groupstats.py
+++ footballpy/analytics/groupstats.py
@@ -20,3 +20,25 @@ def get_team_centroid(pos_data):
     x_centroid = np.nanmean(pos_data[:,::2],1, keepdims = True)
     y_centroid = np.nanmean(pos_data[:,1::2],1, keepdims = True)
     return np.hstack((x_centroid, y_centroid))
+
+def get_team_length_and_width(pos_data):
+    """Calculates the team length and width according.
+
+        Uses the approach described in:
+        Frencken, Lemmink, Delleman & Visscher (2011)
+        Oscillations of centroid position and surface area
+        of soccer teams in small-sided games.
+        European Journal of Sport Science, 11(4), 215-223. 
+
+        Args:
+            pos_data: numpy marix (x X 22) of x-y position data
+        Returns:
+            Matrix (x X s) containing the length [:,0] and
+            width [:,1].
+    """
+    no_frames = pos_data.shape[0]
+    print(no_frames)
+    res = np.zeros((no_frames,2))
+    res[:,0] = np.max(pos_data[:, 0::2], 1) - np.min(pos_data[:, 0::2], 1)
+    res[:,1] = np.max(pos_data[:, 1::2], 1) - np.min(pos_data[:, 1::2], 1)
+    return res
--- footballpy/tests/test_groupstats.py
+++ footballpy/tests/test_groupstats.py
@@ -18,6 +18,7 @@ class TestGetTeamCentroid(unittest.TestCase):
         testData = np.ones((10,22))
         testData[:,0::2] = 10.0
         testData[:,1::2] = 20.0
+
         res = gr.get_team_centroid(testData)
         self.assertTrue(np.all(res[:,0] == 10.0))
         self.assertTrue(np.all(res[:,1] == 20.0))
@@ -30,6 +31,30 @@ class TestGetTeamCentroid(unittest.TestCase):
         testData[0,3] = np.NaN
         testData[7,4] = np.NaN
         testData[8,7] = np.NaN
+
         res = gr.get_team_centroid(testData)
         self.assertTrue(np.all(res[:,0] == 13.3))
         self.assertTrue(np.all(res[:,1] == 27.1))
+
+class TestTeamLengthWidth(unittest.TestCase):
+    """Unit test class for the get_team_length_and_width function.
+    """
+
+    def test_correct_width(self):
+        testData = np.ones((10,22))
+        testData[:,0::2] = 15.0
+        testData[np.arange(10), np.arange(0, 20, 2)] = np.arange(10) * 10.0
+        test_x = np.array([15., 5., 5., 15., 25., 35., 45., 55., 65., 75.])
+
+        res = gr.get_team_length_and_width(testData)
+        self.assertTrue(np.all(res[:,0] == test_x))
+
+    def test_length(self):
+        testData = np.ones((10,22))
+        testData[:,1::2] = 15.0
+        testData[np.arange(10), np.arange(1, 20, 2)] = np.arange(10) * 10.0
+        test_y = np.array([15., 5., 5., 15., 25., 35., 45., 55., 65., 75.])
+
+        res = gr.get_team_length_and_width(testData)
+        self.assertTrue(np.all(res[:,1] == test_y))
+
